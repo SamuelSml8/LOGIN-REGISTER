724c6e4f360be08d0cf258374a1dd2b77fff7ea0
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-21 10:53:34 -0500

    register function finished with jwt and bcrypt

--- src/controllers/emplooyeController.js
+++ src/controllers/emplooyeController.js
@@ -1,7 +1,56 @@
 const Emplooye = require("../models/emplooyeModel.js");
+const bcrypt = require("bcrypt");
 
-const registerEmplooye = async (req, res) => {};
+const registerEmplooye = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const emailExist = await Emplooye.findOne({ email: email });
 
-const loginEmplooye = async (req, res) => {};
+    if (emailExist) {
+      return res.status(400).json({
+        ok: false,
+        message: "Email already exist",
+        data: null,
+      });
+    }
+
+    const emplooyes = await Emplooye.find();
+
+    const emplooyeData = {
+      userId: emplooyes.length + 1,
+      name: name,
+      email: email,
+      password: await bcrypt.hash(password, 10),
+    };
+
+    const newEmplooye = await Emplooye(emplooyeData);
+    const savedEmplooye = await newEmplooye.save();
+
+    res.status(201).json({
+      ok: true,
+      message: "Emplooye registered succesfully",
+      data: savedEmplooye,
+    });
+  } catch (error) {
+    console.log("Error ____ emplooye ", error);
+    res.status(500).json({
+      ok: false,
+      message: "Error Internal Server",
+      data: null,
+    });
+  }
+};
+
+const loginEmplooye = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("Error ____ emplooye ", error);
+    res.status(500).json({
+      ok: false,
+      message: "Error Internal Server",
+      data: null,
+    });
+  }
+};
 
 module.exports = { registerEmplooye, loginEmplooye };
---
+++ src/middlewares/auth.js
@@ -0,0 +1,41 @@
+const passport = require("passport");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+const Emplooye = require("../models/emplooyeModel.js");
+
+const jwt_Secret = ".*#/$=1sMl*!$%&/($DF";
+
+const strategy = new Strategy(
+  {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: jwt_Secret,
+  },
+  async (jwtPayload, done) => {
+    try {
+      const emplooyeFound = await Emplooye.findById({ userId: jwtPayload.id });
+
+      if (!emplooyeFound) {
+        const error = new Error("User not found");
+        console.log(error);
+      }
+
+      done(null, emplooyeFound);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
+
+passport.use(strategy);
+
+const initialize = () => {
+  return passport.initialize();
+};
+
+const authenticate = () => {
+  return passport.authenticate("jwt", { session: false });
+};
+
+module.exports = {
+  initialize,
+  authenticate,
+};
